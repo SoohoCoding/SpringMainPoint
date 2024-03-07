10776fa439a7e9f2d292ecc4c2c756d9f426f294
lch94 <ckdgns1447@gmail.com>
2024-03-07 13:52:54 +0900

    Appconfig 통해서 관심사를 확실하게 분리했다

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,17 +3,17 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
 
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService(); // appConfig에서 memberService를 가져옴
+        OrderService orderService = appConfig.orderService();// appConfig에서 orderService를 가져옴
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
